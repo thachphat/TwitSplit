079f58ec97abb299de37c58f1b83ef2d892f7840
Phat Chiem <thachphat0@gmail.com>
2017-07-14 01:23:24 +0700

    init project

---
+++ TwitSplit/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  TwitSplit
+//
+//  Created by Phat Chiem on 7/14/17.
+//  Copyright © 2017 Phat Chiem. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
