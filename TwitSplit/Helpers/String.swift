a256bd86dc80be7b031bc6bc262c37a09d3185bf
Phat Chiem <thachphat0@gmail.com>
2017-07-18 21:45:02 +0700

    passed test splitMessage > 50 characters

--- TwitSplit/Helpers/String.swift
+++ TwitSplit/Helpers/String.swift
@@ -12,10 +12,62 @@ extension String {
     
     static let messageMaxLength = 50
     
-    static func splitMessage(message: String, result: [String] = []) -> [String] {
-        if result.count == 0 && message.characters.count <= messageMaxLength {
+    static func splitMessage(message: String) -> [String] {
+        if message.characters.count <= messageMaxLength {
             return [message]
         }
-        return [""]
+        
+        let maxPartsDigitCount = findMaxPartsDigitCount(message: message, length: messageMaxLength)
+        let result = splittedString(string: message, length: messageMaxLength, maxPartsDigitCount: maxPartsDigitCount)
+        
+        if let check = result.first(where: { $0.characters.count > messageMaxLength }) {
+            print("Check", check)
+            fatalError("There is at least one part longer than \(messageMaxLength) characters")
+        }
+        
+        return result
+    }
+    
+    fileprivate static func findMaxPartsDigitCount(message: String, length: Int) -> Int {
+        var count = 0
+        var maxCharacters = 0
+        repeat {
+            let A = length - 2 * (count + 2)
+            let B = Int(String(repeating: "1", count: count + 1))!
+            var C = 0
+            if count > 0 {
+                for i in 1...count {
+                    C = C * 10 + i
+                }
+            }
+            
+            maxCharacters = 9 * (A * B + C)
+            count += 1
+        } while maxCharacters < message.characters.count
+        return count
+    }
+    
+    fileprivate static func splittedString(string: String, length: Int, maxPartsDigitCount: Int) -> [String] {
+        var result = [String]()
+        
+        var i = 0
+        while i < string.characters.count {
+            let startIndex = string.index(string.startIndex, offsetBy: i)
+            let splitLength = length - maxPartsDigitCount - 2 - String(result.count + 1).characters.count
+            i += splitLength
+            let distance = string.distance(from: startIndex, to: string.endIndex)
+            var endIndex: Index
+            if distance <= splitLength {
+                endIndex = string.index(startIndex, offsetBy: distance)
+                result.append(string[startIndex..<endIndex])
+                break
+            }
+            endIndex = string.index(startIndex, offsetBy: splitLength)
+            result.append(string[startIndex..<endIndex])
+        }
+        
+        result = result.enumerated().map({ "\($0 + 1)/\(result.count) \($1)" })
+        
+        return result
     }
 }
--- TwitSplitTests/TwitSplitTests.swift
+++ TwitSplitTests/TwitSplitTests.swift
@@ -16,22 +16,50 @@ class TwitSplitTests: XCTestCase {
         let splitMessages = String.splitMessage(message: oneCharacterMessage)
         XCTAssert(splitMessages == [oneCharacterMessage], "Split message with one character")
         
-        let _50CharactersMessage = Array(repeatElement("a", count: 50)).joined()
+        let _50CharactersMessage = String(repeating: "a", count: 50)
         XCTAssert(String.splitMessage(message: _50CharactersMessage) == [_50CharactersMessage], "Split message with 50 characters")
     }
     
     func testSplitMessageGreaterThan50CharactersAndLessThanOrEqual9Splits() {
-        let _51CharactersMessage = Array(repeatElement("a", count: 51)).joined()
-        let firstSplitMessage = "1/2 " + Array(repeatElement("a", count: 46)).joined()
-        let secondSplitMessage = "2/2 " + Array(repeatElement("a", count: 5)).joined()
+        let _51CharactersMessage = String(repeating: "a", count: 51)
+        let firstSplitMessage = "1/2 " + String(repeating: "a", count: 46)
+        let secondSplitMessage = "2/2 " + String(repeating: "a", count: 5)
         XCTAssert(String.splitMessage(message: _51CharactersMessage) == [firstSplitMessage, secondSplitMessage], "Split message with 51 characters")
         
-        let _9SplitsMessage = Array(repeatElement("a", count: 46 * 9)).joined()
-        var _9Splits = Array(repeatElement("/9 " + Array(repeatElement("a", count: 46)).joined(), count: 9))
+        let _9SplitsMessage = String(repeating: "a", count: 46 * 9)
+        var _9Splits = Array(repeatElement(String(repeating: "a", count: 46), count: 9))
         _9Splits = _9Splits.enumerated().map { (index, element) -> String in
-            return "\(index + 1)" + element
+            return "\(index + 1)/\(_9Splits.count) " + element
         }
         XCTAssert(String.splitMessage(message: _9SplitsMessage) == _9Splits, "Split message with 9 splits, 46 characters each")
+    }
+    
+    func testSplitMessageGreaterThan9Splits() {
+        let _10SplitsMessage = String(repeating: "a", count: 46 * 9 + 1)
+        var _10Splits = Array(repeatElement(String(repeating: "a", count: 45), count: 9))
+        _10Splits.append(String(repeating: "a", count: 10))
+        _10Splits = _10Splits.enumerated().map { (index, element) -> String in
+            return "\(index + 1)/\(_10Splits.count) " + element
+        }
+        XCTAssert(String.splitMessage(message: _10SplitsMessage) == _10Splits, "Split message with 10 splits, 45 characters each and last one is 10 characters")
+        
+        let _99SplitsMessage = String(repeating: "a", count: 45 * 9 + 44 * 90)
+        var _99Splits = Array(repeatElement(String(repeating: "a", count: 45), count: 9))
+        _99Splits += Array(repeatElement(String(repeating: "a", count: 44), count: 90))
+        _99Splits = _99Splits.enumerated().map { (index, element) -> String in
+            return "\(index + 1)/\(_99Splits.count) " + element
+        }
+        XCTAssert(String.splitMessage(message: _99SplitsMessage) == _99Splits, "Split message with 99 splits")
+        
+        let _102SplitsMessage = String(repeating: "a", count: 45 * 9 + 44 * 90 + 1)
+        var _102Splits = Array(repeatElement(String(repeating: "a", count: 44), count: 9))
+        _102Splits += Array(repeatElement(String(repeating: "a", count: 43), count: 90))
+        _102Splits += Array(repeatElement(String(repeating: "a", count: 42), count: 2))
+        _102Splits.append(String(repeating: "a", count: 16))
+        _102Splits = _102Splits.enumerated().map { (index, element) -> String in
+            return "\(index + 1)/\(_102Splits.count) " + element
+        }
+        XCTAssert(String.splitMessage(message: _102SplitsMessage) == _102Splits, "Split message with > 100 splits")
     }
     
 }
